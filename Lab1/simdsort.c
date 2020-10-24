f3f0742d6fb7077fa1ec8ea35d9bad270a3a2823
Sebastian Garay <sgarayp@ripley.com>
2020-10-24 20:14:04 -0300

    Added getopt

--- Lab1/simdsort.c
+++ Lab1/simdsort.c
@@ -1,7 +1,33 @@
-# include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
 
-
-int main(){
-    
-    printf("\nFunciona!\n");
+int main(int argc, char **argv){
+    int N, aux;
+    int debugFlag = 0;
+    char *inputFile, *outputFile = NULL;
+    while ((aux = getopt (argc, argv, "i:o:N:d:")) != -1){
+		switch (aux){
+			case 'i':
+				inputFile = optarg;
+				break;
+			case 'o':
+				outputFile = optarg;
+				break;
+			case 'N':
+				N = atoi(optarg);
+				break;
+			case 'd':
+				debugFlag = atoi(optarg);
+				break;
+			default:
+				abort();
+		}
+	}
+    printf("Input File: %s\n",inputFile);
+    printf("Output File: %s\n",outputFile);
+    printf("N: %i\n",N);
+    printf("Debug flag: %i\n",debugFlag);
+        
 }
