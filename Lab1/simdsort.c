1277df49bf357c11d1d2e1d53a77f58d17ee9512
Sebastian Garay <sgarayp@ripley.com>
2020-10-28 20:10:43 -0300

    Fixed bmn

--- Lab1/simdsort.c
+++ Lab1/simdsort.c
@@ -65,19 +65,21 @@ int main(int argc, char **argv){
     */
 
 
+	//float data[] __attribute__ ((aligned (16))) = {12,21,4,13,9,8,6,7,1,14,3,0,5,11,15,10};
 	float data[] __attribute__ ((aligned (16))) = {12,21,4,13,9,8,6,7,1,14,3,0,5,11,15,10};
 	//float data[] __attribute__ ((aligned (16))) = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 
 	int N = 16;
 	// Se hace el proceso cada 16 datos
 	for(int i = 0; i<N; i=i+16){
-		__m128 reg1, reg2, reg3, reg4, maxVector, minVector;
+		__m128 reg1, reg2, reg3, reg4, maxVector, minVector, maxVector2, minVector2;
 		// Se divide la data en 4 arreglos
 		float arr1[4] __attribute__ ((aligned (16))) = {data[i],data[i+1],data[i+2],data[i+3]};
 		float arr2[4] __attribute__ ((aligned (16))) = {data[i+4],data[i+5],data[i+6],data[i+7]};
 		float arr3[4] __attribute__ ((aligned (16))) = {data[i+8],data[i+9],data[i+10],data[i+11]};
 		float arr4[4] __attribute__ ((aligned (16))) = {data[i+12],data[i+13],data[i+14],data[i+15]};
 		float printvector[4] __attribute__ ((aligned (16)));
+		printf("Original: \n");
 		printArr(arr1, 4);
 		printArr(arr2, 4);
 		printArr(arr3, 4);
@@ -128,7 +130,7 @@ int main(int argc, char **argv){
 
 
 
-		/*printf("Despues de min max:\n");
+		printf("\nDespues de min max:\n");
 		 _mm_store_ps(arr1, reg1);
 		 _mm_store_ps(arr2, reg2);
 		 _mm_store_ps(arr3, reg3);
@@ -136,7 +138,7 @@ int main(int argc, char **argv){
 		printArr(arr1, 4);
 		printArr(arr2, 4);
 		printArr(arr3, 4);
-		printArr(arr4, 4); */
+		printArr(arr4, 4);
 
 		__m128 A = _mm_shuffle_ps(reg1, reg3, _MM_SHUFFLE(1,0,1,0));
 			   A = _mm_shuffle_ps(A,A,_MM_SHUFFLE(3,1,2,0));
@@ -167,7 +169,7 @@ int main(int argc, char **argv){
 		__m128 D1 = _mm_shuffle_ps(B, D, _MM_SHUFFLE(3,2,3,2));
 			   D1 = _mm_shuffle_ps(D1,D1,_MM_SHUFFLE(3,1,2,0));
 	
-	 /*printf("Despues de min max:\n");
+	 	printf("\nDespues de transponer:\n");
 		 _mm_store_ps(arr1, A1);
 		 _mm_store_ps(arr2, B1);
 		 _mm_store_ps(arr3, C1);
@@ -175,80 +177,82 @@ int main(int argc, char **argv){
 		printArr(arr1, 4);
 		printArr(arr2, 4);
 		printArr(arr3, 4);
-		printArr(arr4, 4);  */ 
+		printArr(arr4, 4);
 
 		// => A1 B1
 		// => 
 
 	
-	
-
+		/////////////
+		// SHUFFLE //
+		/////////////
+		// 2 primeras filas
+		// Se invierte el 2do registro
+		B1 = _mm_shuffle_ps(B1, B1, _MM_SHUFFLE(0,1,2,3));
+		
+		// Primera etapa
 		__m128  B2= _mm_shuffle_ps(B1, B1, _MM_SHUFFLE(3,1,2,0));
 		__m128  A2= _mm_shuffle_ps(A1, A1, _MM_SHUFFLE(3,1,2,0));
+		minVector = _mm_max_ps(A2,B2);
+		maxVector = _mm_min_ps(A2,B2);
 
-		maxVector = _mm_max_ps(A2,B2);
-		minVector = _mm_min_ps(A2,B2);
-
-			
-		// BIEN ! // 
-
-
+		// Segunda etapa
 		__m128   Max1  = _mm_shuffle_ps(maxVector, minVector, _MM_SHUFFLE(2,0,2,0));
 		__m128   Min1  = _mm_shuffle_ps(maxVector, minVector, _MM_SHUFFLE(3,1,3,1));
+		Max1  = _mm_shuffle_ps(Max1, Max1, _MM_SHUFFLE(2,0,3,1));
+		Min1  = _mm_shuffle_ps(Min1, Min1, _MM_SHUFFLE(2,0,3,1));
+		minVector = _mm_max_ps(Max1,Min1);
+		maxVector = _mm_min_ps(Max1,Min1);
 
-	
-		 Max1  = _mm_shuffle_ps(Max1, Max1, _MM_SHUFFLE(2,0,3,1));
-		 Min1  = _mm_shuffle_ps(Min1, Min1, _MM_SHUFFLE(2,0,3,1));
-
-	
-		maxVector = _mm_max_ps(Max1,Min1);
-		minVector = _mm_min_ps(Max1,Min1);
-
-
-		
-
-		// [5,7,6,8]// SHUFFLE
-		// [4,2,3,1] // SHUFFLE
-	
-
-		// [5,4,6,3]
-		// [7,2,8,1]
-
-
-
-
-		// [7,4,8,3]
-		// [5,2,6,1]
-
-						/*
-printf("Despues de min max:\n");
-			 _mm_store_ps(arr1, maxVector);
-		 _mm_store_ps(arr2, minVector);
-
-		printArr(arr1, 4);
-		printArr(arr2, 4); */
-		
-
+		// Tercera etapa
 		__m128   Max2  = _mm_shuffle_ps(maxVector, minVector, _MM_SHUFFLE(1,0,1,0));
 		__m128   Min2  = _mm_shuffle_ps(maxVector, minVector, _MM_SHUFFLE(3,2,3,2));
-
-						
-
 		Max2  = _mm_shuffle_ps(Max2, Max2, _MM_SHUFFLE(3,1,2,0));
 		Min2  = _mm_shuffle_ps(Min2, Min2, _MM_SHUFFLE(3,1,2,0));
+		minVector2 = _mm_max_ps(Max2,Min2);
+		maxVector2 = _mm_min_ps(Max2,Min2);
+		
+		// 2 ultimas filas
+		// Se invierte el 2do registro
+		D1 = _mm_shuffle_ps(D1, D1, _MM_SHUFFLE(0,1,2,3));
+
+		// Primera etapa
+		__m128  D2= _mm_shuffle_ps(D1, D1, _MM_SHUFFLE(3,1,2,0));
+		__m128  C2= _mm_shuffle_ps(C1, C1, _MM_SHUFFLE(3,1,2,0));
+		minVector2 = _mm_max_ps(C2,D2);
+		maxVector2 = _mm_min_ps(C2,D2);
+
+		// Segunda etapa
+		__m128   Max3  = _mm_shuffle_ps(maxVector2, minVector2, _MM_SHUFFLE(2,0,2,0));
+		__m128   Min3  = _mm_shuffle_ps(maxVector2, minVector2, _MM_SHUFFLE(3,1,3,1));
+		Max3  = _mm_shuffle_ps(Max3, Max3, _MM_SHUFFLE(2,0,3,1));
+		Min3  = _mm_shuffle_ps(Min3, Min3, _MM_SHUFFLE(2,0,3,1));
+		minVector2 = _mm_max_ps(Max3,Min3);
+		maxVector2 = _mm_min_ps(Max3,Min3);
+
+		// Tercera etapa
+		__m128   Max4  = _mm_shuffle_ps(maxVector2, minVector2, _MM_SHUFFLE(1,0,1,0));
+		__m128   Min4  = _mm_shuffle_ps(maxVector2, minVector2, _MM_SHUFFLE(3,2,3,2));
+		Max4  = _mm_shuffle_ps(Max4, Max4, _MM_SHUFFLE(3,1,2,0));
+		Min4  = _mm_shuffle_ps(Min4, Min4, _MM_SHUFFLE(3,1,2,0));
+		minVector2 = _mm_max_ps(Max4,Min4);
+		maxVector2 = _mm_min_ps(Max4,Min4);
+
+		
 
 
-
-		maxVector = _mm_max_ps(Max2,Min2);
-		minVector = _mm_min_ps(Max2,Min2);
-
-
-printf("Despues de min max:\n");
-			 _mm_store_ps(arr1, maxVector);
-		 _mm_store_ps(arr2, minVector);
-
+		printf("\n\n\n");
+		_mm_store_ps(arr1, maxVector);
+		_mm_store_ps(arr2, minVector);
 		printArr(arr1, 4);
 		printArr(arr2, 4);
+		_mm_store_ps(arr1, maxVector2);
+		_mm_store_ps(arr2, minVector2);
+		printArr(arr1, 4);
+		printArr(arr2, 4);
+
+
+
 
 			
 
